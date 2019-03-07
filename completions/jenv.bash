2e1bc67b01dfa1e8d92f43807d2e97533203ebed
jalvar08 <jeovanny.alvarez@gmail.com>
2019-03-07 11:43:15 -0500

    Fix Syntax Error
    Currently receiving the following syntax error when running `eval "$(jenv init -)"`:
    -bash: /usr/local/Cellar/jenv/0.5.2/libexec/libexec/../completions/jenv.bash: line 8: syntax error near unexpected token `('
    -bash: /usr/local/Cellar/jenv/0.5.2/libexec/libexec/../completions/jenv.bash: line 8: `    local words=("${COMP_WORDS[@]}")'
    
    Removing the unnecessary parenthesis resolves the syntax error.

--- completions/jenv.bash
+++ completions/jenv.bash
@@ -5,7 +5,7 @@ _jenv() {
   if [ "$COMP_CWORD" -eq 1 ]; then
     COMPREPLY=( $(compgen -W "$(jenv commands)" -- "$word") )
   else
-    local words=("${COMP_WORDS[@]}")
+    local words="${COMP_WORDS[@]}"
     unset words[0]
     local completions=$(jenv completions "${words[@]}")
     COMPREPLY=( $(compgen -W "$completions" -- "$word") )
